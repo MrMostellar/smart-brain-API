5b29fb0ed5708ce8fa45c5e65dd3cccae325d7a2
Andrew M <mr.mostellar@gmail.com>
2023-03-13 23:20:57 -0600

    Adding endpoints to the server

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
 const express = require('express');
 
+const database = {
+    users:[
+        {
+            name: 'Andrew',
+            email: 'abc123@gmail.com',
+            password: 'password',
+            entries: 0
+        },
+        {
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'password2',
+            entries: 5
+        }
+    ]
+}  
+
 const app = express();
+app.use(express.json());
+app.use(express.text());
+// app.use(express.urlencoded({extended: false}));
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.json('success');
+    } else{
+        res.status(400).json('error signing in..');
+    }
+});
+
     //request access by submitting a new user, email, and password
-    //post/request search score
+    //post/request entry score
+
 app.listen(3000);
